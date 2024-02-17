640b306dac0b430252cfb26ccf965e8323d3b617
Yailene Alvarez <yailene.alvarez@gmail.com>
2024-02-17 15:09:04 -0500

    created GET&PUT routes on server-side

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,14 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../modules/pool.js');
 
 // PUT /gallery/like/:id
-router.put('/like/:id', (req, res) => {
-  // code here
+router.put('/api/gallery/like/:id', (req, res) => {
+  console.log('PUT request for id:', req.params.id);
+  let sqlText = `UPDATE "gallery" SET "likes"="likes" + 1 WHERE "id"=$1;`;
+
+  pool.query(sqlText, [req.params.id])
+  .then((result) => {
+        console.log(`PUT query to update "likes" was successful: ${sqlText}`);
+        res.send(result.rows);
+  })
+  .catch((err) => {
+        console.log(`Error making PUT query`, err);
+        res.sendStatus(500);
+  })
 });
 
 // GET /gallery
 router.get('/', (req, res) => {
-  // code here
+  let queryText = `SELECT * from "gallery"`;
+
+  pool.query(queryText)
+  .then((result) => {
+      console.log(`GET database query was successful: ${queryText}`)
+      res.send(result.rows);
+  }).catch((err) => {
+      console.log(`Error making GET query,`, err);
+      res.sendStatus(500);
+  })
 });
 
 module.exports = router;
