03be73347ef4f01cd9ef992192dba8bb0daf871e
Yailene Alvarez <yailene.alvarez@gmail.com>
2024-02-18 09:45:23 -0500

    made edits to toggle event listener function & all tests passed

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -8,27 +8,32 @@ function GalleryItem(props) {
 //props.title
 //props.description
 //props.likes
+const [toggle, setToggle] = useState(true);
 
 const handleLikeBtn = (id) => {
     
     axios.put(`/api/gallery/like/${props.id}`)
         .then(response => {
-        console.log('PUT request was successful:', response);
-        props.fetchGallery()
+            console.log('PUT request was successful:', response);
+            props.fetchGallery()
         })  
         .catch(err => {
             console.log('PUT error in updating "likes" value:', err);
         })
 }
 
+const handleToggleImage = () => {
+    setToggle(!toggle);
+}
+
     return (
         <div className="photo" data-testid="galleryItem">
             <h3>{props.title}</h3>
             <span className="gallery-frame">
-            {props.url ? <img className="single_img" src={props.url}></img> : <p>{props.description}</p>}
-
+                {toggle ? <img className="single_img" src={props.url}></img> : <p>{props.description}</p>}
 
             </span>
+            <button className="toggle-btn" data-testid="toggle" onClick={handleToggleImage}>Toggle Image</button>
             <button className="like_btn" data-testid="like" onClick={() => handleLikeBtn(props.id)}>Like 👍</button>
             <p>number of likes: {props.likes}</p>
 
